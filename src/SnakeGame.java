006cf5fbfd73395590aca1dd711c3148a38556fe
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-04-06 15:56:01 +0530

    Update SnakeGame.java

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class SnakeGame {
-
+public class SnakeGame extends JPanel{
+	int boardWidth;
+	int boardHeight;
+	
+	SnakeGame(int boardWidth, int boardHeight){
+		this.boardWidth = boardWidth;
+		this.boardHeight = boardHeight;
+		setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+		setBackground(Color.black);
+		
+	}
 }
